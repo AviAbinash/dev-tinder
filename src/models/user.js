1683d023e58cf48723eb58448c2ab8db40911aba
Abinash <abinash.tripathy@speridian.com>
2025-05-13 20:58:57 +0530

    user modal added

--- src/models/user.js
+++ src/models/user.js
@@ -1,50 +1,67 @@
 const mongoose = require("mongoose");
+const validator = require("validator")
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 50,
-  },
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 50,
+      maxLength: 4,
+    },
 
-  lastName: {
-    type: String,
-  },
-  age: {
-    type: Number,
-    min: 18,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      if (!["male", "female", "others"].includes(value)) {
-        throw new Error("Gender data is not valid");
+    lastName: {
+      type: String,
+      minLength: 50,
+      maxLength: 4,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate(value){
+       if(!validator.isEmail(value)){
+        throw new Error("invalid Email adress")
+       }
       }
     },
+    password: {
+      type: String,
+      required: true,
+    },
+    photoUrl: {
+      type: String,
+      default: "https://cdn-icons-png.flaticon.com/256/149/149071.png",
+        validate(value){
+       if(!validator.isURL(value)){
+        throw new Error("invalid Email adress")
+       }
+      }
+    },
+    about: {
+      type: String,
+      default: "this is a default value of a user",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  emailId: {
-    type: String,
-    required: true,
-    lowercase: true,
-    unique: true,
-    trim: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  photoUrl: {
-    type: String,
-    default: "https://cdn-icons-png.flaticon.com/256/149/149071.png",
-  },
-  about: {
-    type: String,
-    default: "this is a default value of a user",
-  },
-  skills: {
-    type: [String],
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
