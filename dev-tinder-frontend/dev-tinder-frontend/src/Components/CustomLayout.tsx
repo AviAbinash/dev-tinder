19aca188fb20b772b22cf25af26d1a5e2cdb7067
Abinash <abinash.tripathy@speridian.com>
2025-05-21 00:03:04 +0530

    routing and layout added

---
+++ dev-tinder-frontend/dev-tinder-frontend/src/Components/CustomLayout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+const CustomLayout = () => {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+};
+
+export default CustomLayout;
